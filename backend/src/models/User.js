604d751ababec8d608cba940e89587e9cd69853a
yiwang0w0 <77381968+yiwang0w0@users.noreply.github.com>
2025-07-11 17:24:47 +0900

    Revert "Add admin CRUD interface"

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, default: 'user' },
   refreshToken: { type: String, default: '' },
   createdAt: { type: Date, default: Date.now }
 });
